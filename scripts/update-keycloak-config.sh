cfaac16f6ca62f10333ed283bc249f707e9a7a3d
Lars Kellogg-Stedman <lars@oddbit.com>
2026-04-19 23:16:00 -0400

    Use fully qualified image name

--- scripts/update-keycloak-config.sh
+++ scripts/update-keycloak-config.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-podman run --rm --env-file .env -v ./keycloak/config:/config:z adorsys/keycloak-config-cli \
+podman run --rm --env-file .env -v ./keycloak/config:/config:z docker.io/adorsys/keycloak-config-cli \
   --import.var-substitution.enabled=true
